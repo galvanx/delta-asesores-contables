7894fd3c20fead82329450a3d2dddb7976e01aaa
Jorge Galván <jgalvanx@gmail.com>
2025-06-24 16:45:53 -0600

    correo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="h-4 w-4 text-brand" />
-                <span className="text-gray-300">info@contaexpert.com</span>
+                <span className="text-gray-300">info@deltaasesores.com.mx</span>
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="h-4 w-4 text-brand" />
