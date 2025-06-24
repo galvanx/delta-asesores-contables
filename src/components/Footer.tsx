7201fc8504b2b9b085440d95dc24c80e55ea3c34
Jorge Galván <jgalvanx@gmail.com>
2025-06-24 16:41:47 -0600

    Numero Actualizado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
             <div className="space-y-2">
               <div className="flex items-center space-x-3">
                 <Phone className="h-4 w-4 text-brand" />
-                <span className="text-gray-300">+52 (55) 1234-5678</span>
+                <span className="text-gray-300">+52 (81) 8315-3956</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="h-4 w-4 text-brand" />
